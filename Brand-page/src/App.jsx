11a699b4737da42cc3e33d7f51eb65b41c1e48b8
Kunnaltyagi <113753558+123kunnal@users.noreply.github.com>
2025-07-19 17:31:08 +0000

    brand page

---
+++ Brand-page/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <nav>
+        <div className='logo'>
+          <img src="/images/" alt="" srcset="" />
+        </div>
+
+    </nav>
+        </div>
+  )
+}
+
+export default App
